237e2fa3e59e035083d25abd63e584d6a8bdeb74
Sigil-Wen <sigil.w3n@gmail.com>
2020-11-30 10:50:06 -0500

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by sigil wen on 2020-11-30.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
